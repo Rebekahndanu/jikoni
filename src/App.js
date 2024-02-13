0c5099ff459a9d93530f38d442174c0fb8359c8a
Michael <michaelmwendwamumo@gmail.com>
2024-02-13 14:29:03 +0300

    Added search to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-
-
-function App() {
-=======
 import Search from './components/Search';
 
+
 function App() {
-  return (
-    <div className="App">
-      <Search />
->>>>>>> fffea9a (Modified Search.js)
 
   const [recipes, setRecipes] = useState([]);
   
@@ -31,6 +23,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route exact path='/' element={<Home images={images}/>}/>
+        <Route exact path='/search' element={<Search/>}/>
       </Routes>
     </BrowserRouter>
   );
