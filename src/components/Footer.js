93f4a284b0a257e2273add65cb9b3e7b60769e55
nandabi <valerie.nandabi@student.moringaschool.com>
2024-02-14 22:31:25 +0300

    commit message

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+export default function footer () {
+   return (
+    <div className="footer">
+        <div className="footer-section">
+            <p className="title">Jikoni.com</p>
+            <p>The fabric of Jikoni was born ot of love and respect for these humble deli
+            creations,met with desire to bring quality ingredients to the table.Simply 
+            put we are here to bring you the best swahili dishes.</p>
+            <p>&copy; 2024 | All Rights Reserved </p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Contact Us</p>
+            <p>jikoni@gmail.com</p>
+            <p>+254 784803430</p>
+            <p>600 S,Brookhurst,Anaheim</p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Jikoni.com</p>
+            <p>Instagram</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+         </div>
+    </div>
+   )
+
+}
+
+
